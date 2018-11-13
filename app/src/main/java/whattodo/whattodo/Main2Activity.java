315239a5e67108153c5cb6b8d176559e1f278db1
Wojciech <wojciech.dywan@interia.pl>
2018-11-13 17:02:34 +0100

    Preferencje

--- app/src/main/java/whattodo/whattodo/Main2Activity.java
+++ app/src/main/java/whattodo/whattodo/Main2Activity.java
@@ -5,14 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
 public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
-    Button btnLogout, btnFind;
-    TextView TextUsername;
+    private Button btnLogout, btnFind;
+    private CheckBox checkFood, checkAlcohol, checkActive, checkTourism, checkGroup;
+    private SeekBar seekBar;
+    private EditText etPrice;
+    private TextView TextUsername, textDistance;
 
 
     @Override
@@ -20,11 +26,19 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        btnLogout = (Button) findViewById(R.id.btnLogout);
-        btnFind = (Button) findViewById(R.id.btnFind);
+        btnLogout =  findViewById(R.id.btnLogout);
+        btnFind =  findViewById(R.id.btnFind);
         btnLogout.setOnClickListener(this);
         btnFind.setOnClickListener(this);
-        TextUsername = (TextView) findViewById(R.id.etUsername);
+        TextUsername = findViewById(R.id.etUsername);
+        seekBar = findViewById(R.id.seekBar);
+        textDistance = findViewById(R.id.textViewDistance);
+        etPrice = findViewById(R.id.etPrice);
+        checkFood = findViewById(R.id.checkFood);
+        checkAlcohol = findViewById(R.id.checkAlcohol);
+        checkActive = findViewById(R.id.checkActive);
+        checkTourism = findViewById(R.id.checkTourism);
+        checkGroup = findViewById(R.id.checkGroup);
 
         Intent intent = getIntent();
         String username = intent.getStringExtra("username");
@@ -32,6 +46,7 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
         String massage = "Witaj "+username;
         TextUsername.setText(massage);
 
+        seekbarr();
     }
 
     @Override
@@ -41,6 +56,7 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onClick(View v) {
+
         switch (v.getId()){
             case R.id.btnLogout:
 
@@ -48,7 +64,33 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
                 break;
             case R.id.btnFind:
                 startActivity(new Intent(this,MapsActivity.class));
+            //    Toast.makeText(Main2Activity.this, "Check box:"+checkFood.isChecked(), Toast.LENGTH_SHORT).show();
                 break;
         }
     }
+
+
+    public void seekbarr(){
+        seekBar.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    int progres_value;
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        progres_value = progress+1;
+                        textDistance.setText(progres_value+" Km");
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                        textDistance.setText(progres_value+" Km");
+                    }
+                }
+        );
+    }
+
 }
